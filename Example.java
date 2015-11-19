5f88bbd05ccaf310e0a1dfcd1c0aae89dc97f038
Linux Desktop <shuler.david.p@gmail.com>
2015-11-19 14:06:02 -0600

    1st commit from Desktop

---
+++ Example.java
@@ -0,0 +1,11 @@
+/*
+ * 	This is sample program.
+ *
+ * 	Call this file Example.java
+ */
+class Example {
+// A java program begins with a call to main().
+  public static void main (String args[]) {
+    System.out.println("Java drives the web.");
+  }
+}
